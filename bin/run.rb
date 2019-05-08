110aa7777b2277e0f9d6e5f4cf20b0ed76a3a8b0
github username <github email address>
2019-05-08 16:42:06 -0700

    minor changes

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,8 @@
 require_relative '../config/environment'
+require_relative "../lib/api_communicator.rb"
+require_relative "../lib/command_line_interface.rb"
 
-puts "hello world"
+welcome
+user_response = get_answer_from_user
+#show_questions_and_answers(user_response)
+show_questions_and_answers
