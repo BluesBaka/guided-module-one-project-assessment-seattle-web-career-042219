ce3fce3c38bf2c5e2ba1a7d378bc095310e8a6f0
jjsmokovich <JesseSmokovich@yahoo.com>
2019-05-09 16:41:48 -0700

    added stuff

---
+++ db/migrate/20190509212235_create_table_questions.rb
@@ -0,0 +1,14 @@
+class CreateTableQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :questions do |t| 
+      t.string :category
+      t.string :q_type 
+      t.string :difficulty 
+      t.string :question
+      t.string :correct_answer 
+      t.string :incorrect_answer_1
+      t.string :incorrect_answer_2
+      t.string :incorrect_answer_3
+    end
+  end
+end
